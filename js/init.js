56a7ab80434b8859d7119c64d44e3fa677736c86
kungphanith <kungphanith79@gmail.com>
2016-03-03 01:15:39 +0700

    update scrollfire and project link

--- js/init.js
+++ js/init.js
@@ -6,3 +6,14 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var options = [
+  {selector: '#header-text', offset: 50, callback: 'Materialize.fadeInImage("#header-text")' },
+  {selector: '#image-2', offset: 50, callback: 'Materialize.fadeInImage("#image-2")' },
+  {selector: '#about-content', offset: 50, callback: 'Materialize.fadeInImage("#about-content")' },
+  
+  // {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
+  // {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+  // {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
+];
+Materialize.scrollFire(options);
